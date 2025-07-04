e6525bafa2c11f0685aebb607d3506255bb53301
AkshayJos <akshayjoshi1018@gmail.com>
2025-07-04 23:04:40 +0530

    Fix: added0 0

--- src/main/java/com/example/SafeTradeX/Security/WebSecurityConfig.java
+++ src/main/java/com/example/SafeTradeX/Security/WebSecurityConfig.java
@@ -57,7 +57,7 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://safetradex.onrender.com"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
